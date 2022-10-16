571f1e476e756ce7f1893ed953a4bd1190b83d29
Wakilat <wakilatadeola@gmail.com>
2022-10-16 12:17:52 -0700

    correction

--- models/state.py
+++ models/state.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.city import City
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    def __init__(self, *args, **kwargs):
-        """
-        init inherited
-        """
-        super().__init__(*args, **kwargs)
-
-    if models.storage_type != "db":
-        @property
-        def cities(self):
-            """getter for cities that return
-            a list of city instance equale to
-            curent state id
-            """
-            list_city = []
-            all_inst_c = models.storage.all(City)
-            for value in all_inst_c.values():
-                if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -15,15 +16,17 @@ def teardown_session(exception):
     storage.close()
 
 
-@app.route('/hbnb_filters/', strict_slashes=False)
+@app.route('/hbnb/', strict_slashes=False)
 def display_html():
     """ Function called with /states route """
     states = storage.all(State)
     amenities = storage.all(Amenity)
+    places = storage.all(Place)
 
-    return render_template('10-hbnb_filters.html',
+    return render_template('100-hbnb.html',
                            states=states.values(),
-                           amenities=amenities.values())
+                           amenities=amenities.values(),
+                           places=places.values())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
