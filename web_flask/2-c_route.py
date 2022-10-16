abb1d347199f739646aa26e77afd6fc308c73c90
Wakilat <wakilatadeola@gmail.com>
2022-10-16 11:59:24 -0700

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""
-flask model
-"""
+""" Script that runs an app with Flask framework """
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb():
-    """
-        / home path
-    """
+def hello_hbnb():
+    """ Function called with / route """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def index():
-    """
-        /hbnb path
-    """
+def hbnb():
+    """ Function called with /hbnb route """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def C_is(text):
-    """
-        /C path
-    """
-    return 'C {:s}'.format(text.replace('_', ' '))
+def c_text(text):
+    """ Function called with /c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000
+    app.run(host='0.0.0.0', port=5000)
